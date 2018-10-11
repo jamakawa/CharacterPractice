2e0ad2897f01778a2584f77a1c2d21f7a02e467c
JonAmakawa <jamakawa@fitchburgstate.edu>
2018-10-10 21:22:00 -0400

    Initial commit

---
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Animation/playerControlsScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControlsScript : MonoBehaviour {
+
+	private Animator ThisAnimator = null;
+
+	// Use this for initialization
+	void Awake () {
+		ThisAnimator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float Horz = Input.GetAxis ("Horizontal");
+		float Vert = Input.GetAxis ("Vertical");
+
+		ThisAnimator.SetFloat ("Forwards", Vert);
+		ThisAnimator.SetFloat ("Turn", Vert);
+	}
+}
